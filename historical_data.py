5c21d1c0da2eb6e1e84e8e16f0f148084df548bb
Rajveersingh Sisodiya <rajveersingh1906@gmail.com>
2026-08-27 23:41:53 +0530

    historical data from api

---
+++ historical_data.py
@@ -0,0 +1,80 @@
+from twelvedata import TDClient
+import pandas as pd
+
+td = TDClient(apikey="5e2d32211d6b4d35a4aeb7324192bc4d")
+
+# 🇮🇳 Indian company
+
+d1 = td.time_series(
+    symbol="INFY",
+    interval="1day",
+    outputsize=5000
+).as_pandas()
+d1["company"] = "INFY"
+
+
+# 🇺🇸 US companies
+
+d2 = td.time_series(
+    symbol="AAPL",
+    interval="1day",
+    outputsize=5000
+).as_pandas()
+d2["company"] = "AAPL"
+
+
+d3 = td.time_series(
+    symbol="MSFT",
+    interval="1day",
+    outputsize=5000
+).as_pandas()
+d3["company"] = "MSFT"
+
+
+d4 = td.time_series(
+    symbol="GOOGL",
+    interval="1day",
+    outputsize=5000
+).as_pandas()
+d4["company"] = "GOOGL"
+
+
+d5 = td.time_series(
+    symbol="AMZN",
+    interval="1day",
+    outputsize=5000
+).as_pandas()
+d5["company"] = "AMZN"
+
+
+d6 = td.time_series(
+    symbol="NVDA",
+    interval="1day",
+    outputsize=5000
+).as_pandas()
+d6["company"] = "NVDA"
+
+
+d7 = td.time_series(
+    symbol="TSLA",
+    interval="1day",
+    outputsize=5000
+).as_pandas()
+d7["company"] = "TSLA"
+
+
+d8 = td.time_series(
+    symbol="META",
+    interval="1day",
+    outputsize=5000
+).as_pandas()
+d8["company"] = "META"
+
+
+df = pd.concat(
+    [d1, d2, d3, d4, d5, d6, d7, d8],
+    ignore_index=True
+)
+
+
+print(df.head())
